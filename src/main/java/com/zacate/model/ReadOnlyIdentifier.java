207da720b3bfca02a5fcd30819c3e3c228ed3d00
Armando Alaniz <arjoalaniz@gmail.com>
2018-06-17 17:47:11 -0600

    Agregar clase base para identificar entidades

---
+++ src/main/java/com/zacate/model/ReadOnlyIdentifier.java
@@ -0,0 +1,48 @@
+package com.zacate.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Armando Alaniz
+ * @version 1.0
+ * @since 1.0
+ */
+public abstract class ReadOnlyIdentifier<T> {
+
+    protected final T id;
+
+    protected ReadOnlyIdentifier(T id) {
+        this.id = id;
+    }
+
+    public T getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 61 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ReadOnlyIdentifier other = (ReadOnlyIdentifier) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/com/zacate/model/WriteableIdentifier.java
@@ -0,0 +1,55 @@
+package com.zacate.model;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Armando Alaniz
+ * @version 1.0
+ * @since 1.0
+ */
+public abstract class WriteableIdentifier<T> {
+
+    protected T id;
+
+    protected WriteableIdentifier() {
+    }
+
+    protected WriteableIdentifier(T id) {
+        this.id = id;
+    }
+
+    public T getId() {
+        return id;
+    }
+
+    public void setId(T id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final WriteableIdentifier other = (WriteableIdentifier) obj;
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return true;
+    }
+
+}
